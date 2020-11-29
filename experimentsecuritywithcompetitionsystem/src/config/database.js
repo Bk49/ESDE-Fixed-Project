0cfdd44cef3c2a137fd27b547e6e545b0039ee74
Bk49 <ericngyongwei@gmail.com>
2020-11-29 09:54:53 +0800

    Promisifying Login
    - Added promise to login API
    - Promisified pool.query => Might break some APIs

--- experimentsecuritywithcompetitionsystem/src/config/database.js
+++ experimentsecuritywithcompetitionsystem/src/config/database.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const config = require('./config');
+const util = require('util')
 //To find out more on createPool:
 //https://www.npmjs.com/package/mysql#pooling-connections
 
@@ -12,4 +13,24 @@ const pool = mysql.createPool({
         multipleStatements: true
     });
 
- module.exports=pool;
+// Ping database to check for common exception errors.
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.')
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connections.')
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused.')
+        }
+    }
+    
+    if (connection) connection.release()
+     return
+    })
+      
+pool.query = util.promisify(pool.query)
+
+module.exports = pool;
--- experimentsecuritywithcompetitionsystem/src/controllers/authController.js
+++ experimentsecuritywithcompetitionsystem/src/controllers/authController.js
@@ -10,53 +10,71 @@ exports.processLogin = (req, res, next) => {
 
     let email = req.body.email;
     let password = req.body.password;
-    try {
-        auth.authenticate(email, function(error, results) {
-            if (error) {
-                let message = 'Credentials are not valid.';
-                //return res.status(500).json({ message: message });
-                //If the following statement replaces the above statement
-                //to return a JSON response to the client, the SQLMap or
-                //any attacker (who relies on the error) will be very happy
-                //because they relies a lot on SQL error for designing how to do 
-                //attack and anticipate how much "rewards" after the effort.
-                //Rewards such as sabotage (seriously damage the data in database), 
-                //data theft (grab and sell). 
-                return res.status(500).json({ message: error });
-
-            } else {
-                if (results.length == 1) {
-                    if ((password == null) || (results[0] == null)) {
-                        return res.status(500).json({ message: 'login failed' });
-                    }
-                    if (bcrypt.compareSync(password, results[0].user_password) == true) {
-
-                        let data = {
-                            user_id: results[0].user_id,
-                            role_name: results[0].role_name,
-                            token: jwt.sign({ id: results[0].user_id }, config.JWTKey, {
-                                expiresIn: 86400 //Expires in 24 hrs
-                            })
-                        }; //End of data variable setup
-
-                        return res.status(200).json(data);
-                    } else {
-                        // return res.status(500).json({ message: 'Login has failed.' });
-                        return res.status(500).json({ message: error });
-                    } //End of passowrd comparison with the retrieved decoded password.
-                } //End of checking if there are returned SQL results
-
+    // try {
+    auth.authenticate(email).then((results)=>{
+        if(results.length == 1){
+            if((password == null) || (results[0] == null)) return res.status(500).json({message: 'login failed'})
+            if(bcrypt.compareSync(password, results[0].user_password) == true){
+                let data = {
+                    user_id: results[0].user_id,
+                    role_name: results[0].role_name,
+                    token: jwt.sign({ id: results[0].user_id }, config.JWTKey, {
+                        expiresIn: 86400 //Expires in 24 hrs
+                    })
+                }
+                return res.status(200).json(data)
             }
+        }else return res.status(500).json({message:'login failed'})
+    }).catch((err) => {
+        console.log(err)
+        res.status(500).json({message:'login failed'})
+    })
+}
+    //         if (error) {
+    //             let message = 'Credentials are not valid.';
+    //             //return res.status(500).json({ message: message });
+    //             //If the following statement replaces the above statement
+    //             //to return a JSON response to the client, the SQLMap or
+    //             //any attacker (who relies on the error) will be very happy
+    //             //because they relies a lot on SQL error for designing how to do 
+    //             //attack and anticipate how much "rewards" after the effort.
+    //             //Rewards such as sabotage (seriously damage the data in database), 
+    //             //data theft (grab and sell). 
+    //             return res.status(500).json({ message: error });
 
-        })
+    //         } else {
+    //             if (results.length == 1) {
+    //                 if ((password == null) || (results[0] == null)) {
+    //                     return res.status(500).json({ message: 'login failed' });
+    //                 }
+    //                 if (bcrypt.compareSync(password, results[0].user_password) == true) {
 
-    } catch (error) {
-        return res.status(500).json({ message: error });
-    } //end of try
+    //                     let data = {
+    //                         user_id: results[0].user_id,
+    //                         role_name: results[0].role_name,
+    //                         token: jwt.sign({ id: results[0].user_id }, config.JWTKey, {
+    //                             expiresIn: 86400 //Expires in 24 hrs
+    //                         })
+    //                     }; //End of data variable setup
+
+    //                     return res.status(200).json(data);
+    //                 } else {
+    //                     // return res.status(500).json({ message: 'Login has failed.' });
+    //                     return res.status(500).json({ message: error });
+    //                 } //End of passowrd comparison with the retrieved decoded password.
+    //             } //End of checking if there are returned SQL results
+
+    //         }
+
+    //     })
+
+    // } catch (error) {
+    //     return res.status(500).json({ message: error });
+    // } //end of try
 
 
 
-};
+// };
 
 // If user submitted data, run the code in below
 exports.processRegister = (req, res, next) => {
